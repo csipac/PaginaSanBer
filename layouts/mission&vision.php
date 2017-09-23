ac0c9b86b7b4de0152bfe17f9f52e8f2abb6cd9f
ech-tin <agutzi@gmail.com>
2017-09-23 13:57:35 -0600

    actualizando

--- layouts/mission&vision.php
+++ layouts/mission&vision.php
@@ -2,40 +2,48 @@
     <div class="container">
         <div class="section-header">
             <h2 class="section-title text-center wow fadeInDown">Nuestra Mision y Vision</h2>
-            <p class="text-center wow fadeInDown">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+            <p class="text-center wow fadeInDown">Estos son los pilares que sustentan nuestra Comunidad Educativa. Principios y Valores compartidos por el alumnado, profesorado y padres de familia.<br>  Todos juntos trabajamos por crear una comunidad centrada tanto en el logro personal, espiritual como social.</p>
         </div>
         <div class="row">
             <div class="col-sm-6 wow fadeInLeft">
                 <img class="img-responsive" src="images/history/valores1.png" alt="">
             </div>
             <div class="col-sm-6">
+
+              <div class="media service-box wow fadeInRight">
+
+                  <div class="pull-left">
+                      <i class="fa fa-eye"></i>
+                  </div>
+                  <div class="media-body">
+                      <h4 class="media-heading">Vision</h4>
+                      <p>Ser una Institución Educativa Católica Franciscana de excelencia......<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#visionmodal">Leer mas</button></p>
+                  </div>
+              </div>
+              <?php include('layouts/modals.php') ?>
+
+
+
+
                 <div class="media service-box wow fadeInRight">
                     <div class="pull-left">
                         <i class="fa fa-rocket"></i>
                     </div>
                     <div class="media-body">
                         <h4 class="media-heading">Mision</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+                        <p> Ofrecer a la Comunidad Eduacativa una formación de calidad......<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#misionmodal">Leer mas</button> </p>
                     </div>
                 </div>
 
-                <div class="media service-box wow fadeInRight">
-                    <div class="pull-left">
-                        <i class="fa fa-eye"></i>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">Vision</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
-                    </div>
-                </div>
+
 
                 <div class="media service-box wow fadeInRight">
                     <div class="pull-left">
                         <i class="fa fa-child"></i>
                     </div>
                     <div class="media-body">
-                        <h4 class="media-heading">Idearios</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
+                        <h4 class="media-heading">Ideario</h4>
+                        <p>Como Institución Educativa Católica Franciscana nuestro IDEARIO.....<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ideariomodal">Leer mas</button></p>
                     </div>
                 </div>
 
