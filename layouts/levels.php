c208a66d89cc9fa561871c6d6beac3cd68077890
csipac <cristhian.sipac@myappsoftware.com>
2017-09-30 12:59:12 -0600

    Columnas corregidas

--- layouts/levels.php
+++ layouts/levels.php
@@ -6,7 +6,7 @@
       </div>
       <div class="row">
           <div class="features">
-              <div class="col-sm-6 col-md-3 col-md-offset-2 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+              <!-- <div class="col-sm-3 col-md-3  wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
                   <div class="features-item">
                       <div class="features-icon">
                           <i class="fa fa-graduation-cap">Kinder</i>
@@ -16,7 +16,7 @@
                   </div>
               </div>
 
-              <div class="col-sm-6 col-md-3 col-lg-3 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+              <div class="col-sm-3 col-md-3 col-md-offset-2 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
                   <div class="features-item">
                       <div class="features-icon">
                           <i class="fa fa-graduation-cap">Parvulos</i>
@@ -26,46 +26,113 @@
                   </div>
               </div>
 
-              <div class="col-sm-6 col-md-3 col-lg-3 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+              <div class="col-sm-6 col-md-3 col-md-offset-1 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
                   <div class="features-item">
                       <div class="features-icon">
                           <i class="fa fa-graduation-cap">Preparatoria</i>
                       </div>
                       <h3 class="features-title font-alt"></h3>
                       Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                  </div>
+                  </div> -->
               </div>
+        </div><!--/.row-->
 
 
-              <div class="col-sm-6 col-md-3 col-md-offset-2 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="500ms">
-                  <div class="features-item">
-                      <div class="features-icon">
-                          <i class="fa fa-graduation-cap">Primaria</i>
-                      </div>
-                      <h3 class="features-title font-alt"></h3>
-                      Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                  </div>
+              <!-- <div class="row">
+
+                <div class="col-sm-3 col-md-3  wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <i class="fa fa-graduation-cap">Kinder</i>
+                        </div>
+                        <h3 class="features-title font-alt"></h3>
+                        Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                    </div>
+                </div>
+
+                <div class="col-sm-3 col-md-3 col-md-offset-2 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <i class="fa fa-graduation-cap">Parvulos</i>
+                        </div>
+                        <h3 class="features-title font-alt"></h3>
+                        Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-md-offset-1 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <i class="fa fa-graduation-cap">Preparatoria</i>
+                        </div>
+                        <h3 class="features-title font-alt"></h3>
+                        Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                    </div>
+                </div>
+            </div> -->
+            <div class="row">
+              <div class="col-md-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
               </div>
 
-              <div class="col-sm-6 col-md-3 col-lg-3 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="600ms">
-                  <div class="features-item">
-                      <div class="features-icon">
-                          <i class="fa fa-graduation-cap">Basico</i>
-                      </div>
-                      <h3 class="features-title font-alt"></h3>
-                      Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                  </div>
+              <div class="col-md-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="600ms">
-                  <div class="features-item">
-                      <div class="features-icon">
-                          <i class="fa fa-graduation-cap">Diversificado</i>
-                      </div>
-                      <h3 class="features-title font-alt"></h3>
-                      Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
-                  </div>
+
+              <div class="col-md-4  wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
               </div>
-          </div>
-      </div><!--/.row-->
+            </div>
+
+            <div class="row">
+              <div class="col-md-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
+              </div>
+
+              <div class="col-md-4 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
+              </div>
+
+              <div class="col-md-4  wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+                <div class="features-item">
+                    <div class="features-icon">
+                        <i class="fa fa-graduation-cap">Preparatoria</i>
+                    </div>
+                    <h3 class="features-title font-alt"></h3>
+                    Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.
+                </div>
+              </div>
+            </div>
+
     </div>
 </section>
