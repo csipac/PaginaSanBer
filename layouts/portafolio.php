f61ad72030dabb134582b398d65a005708d5d384
Cristhian <cristhian.sipac@myappsoftware.com>
2017-12-02 16:45:26 -0600

    Vista modificada 2 de diciembre

--- layouts/portafolio.php
+++ layouts/portafolio.php
@@ -7,116 +7,23 @@
 
         <div class="text-center">
             <ul class="portfolio-filter">
-                <li><a class="active" href="#" data-filter="*">Salon Principal</a></li>
-                <li><a href="#" data-filter=".animation">Biblioteca</a></li>
+
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalsalon">Salon Principal</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalbiblio">Biblioteca</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalcompu">Salon de Computacion</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalcapilla">Capilla</button>
+                <!-- <li><a class="active" href="#" data-filter="*">Salon Principal</a></li> -->
+                <!-- <li><a href="#" data-filter=".animation">Biblioteca</a></li>
                 <li><a href="#" data-filter=".Business">Salon de Computacion</a></li>
-                <li><a href="#" data-filter=".art">Capilla</a></li>
+                <li><a href="#" data-filter=".art">Capilla</a></li> -->
             </ul><!--/#portfolio-filter-->
         </div>
 
-        <div class="portfolio-items">
-            <div class="portfolio-item animation">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/Biblioteca/01.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 1</h3>
-                        <a class="preview" href="images/portfolio/Biblioteca/01.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
+        <?php include('layouts/modalSalon.php') ?>
+        <?php include('layouts/modalBiblioteca.php') ?>
+        <?php include('layouts/modalCompu.php') ?>
+        <?php include('layouts/modalCapilla.php') ?>
 
 
-            <div class="portfolio-item Business art">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/02.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 2</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item animation">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/03.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 3</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item Business">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/04.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 4</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item animation art">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/05.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 5</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item Business">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/06.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 5</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item animation art">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/07.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 7</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-
-            <div class="portfolio-item Business">
-                <div class="portfolio-item-inner">
-                    <img class="img-responsive" src="images/portfolio/08.jpg" alt="">
-                    <div class="portfolio-info">
-                        <h3>Portfolio Item 8</h3>
-                        <a class="preview" href="images/portfolio/full.jpg" rel="prettyPhoto">
-                            <img src="images/portfolio/expand.png" alt="">
-
-                        </a>
-                    </div>
-                </div>
-            </div><!--/.portfolio-item-->
-        </div>
     </div><!--/.container-->
 </section><!--/#portfolio-->
